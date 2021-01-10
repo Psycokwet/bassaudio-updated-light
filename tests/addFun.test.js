9c39d5bad67bbbedff331a715a9083cef2267360
sophie carboni <socarboni@gmail.com>
2021-01-10 16:02:33 +0100

    get back update from base project

--- tests/addFun.test.js
+++ tests/addFun.test.js
@@ -3,33 +3,43 @@ var getBass = require("../tools/getBass");
 const path = require("path");
 var getPlatformDependenciesForOneFile = require("../tools/getPlatformDependenciesForOneFile");
 
-var basslib = getBass({
-  silent: true,
-  generatedFfiFunDeclaration: {
-    bass: {
-      ffiFunDeclaration: {
-        BASS_IsStarted: ["bool", []],
+var basslib;
+test("Bass generation working ? ", () => {
+  expect(() => {
+    basslib = getBass({
+      silent: true,
+      generatedFfiFunDeclaration: {
+        bass: {
+          ffiFunDeclaration: {
+            BASS_IsStarted: ["bool", []],
+          },
+        },
+        webm: getPlatformDependenciesForOneFile(
+          {
+            BASS_WEBM_StreamCreateURL: [
+              "int",
+              ["string", "int", "int", "pointer", "pointer", "int"],
+            ],
+          },
+          path.join(__dirname, "lib", "macOs", "libbasswebm.dylib"),
+          path.join(__dirname, "lib", "win32", "basswebm.dll"),
+          path.join(__dirname, "lib", "win64", "basswebm.dll"),
+          path.join(__dirname, "lib", "linux32", "libbasswebm.so"),
+          path.join(__dirname, "lib", "linux64", "libbasswebm.so")
+        ),
       },
-    },
-    webm: getPlatformDependenciesForOneFile(
-      {
-        BASS_WEBM_StreamCreateURL: [
-          "int",
-          ["string", "int", "int", "pointer", "pointer", "int"],
-        ],
-      },
-      path.join(__dirname, "lib", "macOs", "libbasswebm.dylib"),
-      path.join(__dirname, "lib", "win32", "basswebm.dll"),
-      path.join(__dirname, "lib", "win64", "basswebm.dll"),
-      path.join(__dirname, "lib", "linux32", "libbasswebm.so"),
-      path.join(__dirname, "lib", "linux64", "libbasswebm.so")
-    ),
-  },
+    });
+  }).not.toThrowError();
 });
 
 // const os = require("os");
-// if (os.platform() !== "darwin") { // same folder, no problem on mac
-basslib.EnableWebm(true);
+// if (os.platform() !== "darwin") {
+test("Auto enable all available libs", () => {
+  expect(() => {
+    // basslib.EnableWebm(true);
+    basslib.EnableAllAvailable(true); // I test with it to check that it is effectively added in the auto enable list. But the up line would be as fine.
+  }).not.toThrowError();
+});
 
 test("test full new lib file linking", () => {
   expect(basslib.BASS_WEBM_StreamCreateURL()).toBe(0);
